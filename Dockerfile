ef9122e7e4bc702e3458b1f2f19c18b962086b0e
Andrei Yurkouski <emp74ark@gmail.com>
2025-10-06 17:43:55 +0200

    feat: use crawler microservice

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
-FROM node:22-slim
+FROM node:22-slim as builder
 WORKDIR /app
 COPY package*.json .
 RUN npm install
 COPY . .
 RUN npm run build
+
+
+FROM node:22-slim
+WORKDIR /app
+COPY package*.json .
+RUN npm install --prod
+COPY --from=builder /app/dist ./dist/
+COPY --from=builder /app/.env ./
 EXPOSE 3600
 ENV NODE_ENV=production
-RUN sh ./puppeteer.sh
 CMD ["node", "dist/main.js"]
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FeedParserService } from './feed-parser.service';
+import { CrawlerService } from './crawler.service';
 
 @Module({
   imports: [],
-  providers: [FeedParserService],
-  exports: [FeedParserService],
+  providers: [FeedParserService, CrawlerService],
+  exports: [FeedParserService, CrawlerService],
 })
 export class SharedModule {}
