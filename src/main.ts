0499ea2b9d39ac22e4ce88267450a907c3990bfa
Andrei Yurkouski <emp74ark@gmail.com>
2026-05-17 13:46:18 +0200

    feat: trust proxy and adjust cookie `sameSite` for production

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({ trustProxy: true }),
     { logger },
   );
 
@@ -46,7 +46,7 @@ async function bootstrap() {
     }),
     cookie: {
       httpOnly: true,
-      sameSite: 'lax',
+      sameSite: appConfig.production ? 'none' : 'lax',
       maxAge: sessionTtl,
       secure: appConfig.production,
     },
